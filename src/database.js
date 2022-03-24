2ef70e2a05b96d96fce2a3761a88a9b08e427494
Weed <rivenmle@gmail.com>
2022-03-24 15:52:05 +0700

    Fixed 500 error when multi-connect

--- src/database.js
+++ src/database.js
@@ -1,15 +1,15 @@
 import mysql from "mysql";
 
+var connection = mysql.createConnection({
+	host: process.env.MYSQL_HOST,
+	user: process.env.MYSQL_USER,
+	password: process.env.MYSQL_PASSWORD || "",
+	database: process.env.MYSQL_DATABASE,
+});
+
+connection.connect();
+
 function execute(queryString) {
-	var connection = mysql.createConnection({
-		host: process.env.MYSQL_HOST,
-		user: process.env.MYSQL_USER,
-		password: process.env.MYSQL_PASSWORD || "",
-		database: process.env.MYSQL_DATABASE,
-	});
-
-	connection.connect();
-
 	return new Promise((resolve, reject) => {
 		connection.query(queryString, (error, results) => {
 			if (error) {
@@ -18,8 +18,6 @@ function execute(queryString) {
 			}
 
 			resolve(results);
-
-			connection.end();
 		});
 	});
 }
