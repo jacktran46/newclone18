f8f889b8098ccc211f4d5d465cef4b503c929f7c
Weed <rivenmle@gmail.com>
2022-03-24 15:58:57 +0700

    Changed single connection to pool connection mysql

--- src/database.js
+++ src/database.js
@@ -1,18 +1,12 @@
 import mysql from "mysql";
 
-let initialized = false;
-
-var connection = mysql.createConnection({
+var connection = mysql.createPool({
+	connectionLimit: 100,
 	host: process.env.MYSQL_HOST,
 	user: process.env.MYSQL_USER,
 	password: process.env.MYSQL_PASSWORD || "",
 	database: process.env.MYSQL_DATABASE,
 });
-
-if (!initialized) {
-	connection.connect();
-	initialized = true;
-}
 
 function execute(queryString) {
 	return new Promise((resolve, reject) => {
